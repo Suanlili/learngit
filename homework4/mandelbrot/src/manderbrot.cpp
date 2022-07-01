555d9db4480a2411b6911fedb2bca79cb413171c
suanlili <zlarryz@qq.com>
2022-07-01 21:58:17 +0800

    Ready to add bibtex

--- homework4/mandelbrot/src/manderbrot.cpp
+++ homework4/mandelbrot/src/manderbrot.cpp
@@ -3,36 +3,38 @@
 #include "Window.h"
 #include "Manderbrot.h"
 #include "../../bitmap/src/bitmap.h"
+//水平有限T_T，这里定义了两个全局变量，用于每次迭代调整zoom的位置。
+double o_x, o_y;
 
-int main(int argc, char *argv[])
+/*这里对老师的manderbrot.cpp中的main函数做了一些修改，使其每次执行可以进行一次调整。
+调整使得zoom的位置位于边界附近。
+*/
+int generate(char* bmpname, double oox, double ooy, double indim)
 {
-	if (argc < 5)
-	{
-		std::cerr << "Usage: " << argv[0]
-				  << " filename ox oy dimension" << std::endl;
-		exit(-1);
-	}
+
 	
-	Window win(std::atof(argv[2]), std::atof(argv[3]), std::atof(argv[4]));
+	Window win(oox, ooy, indim);
 	double lpp = win.get_lpp();
 	double dim = win.get_dimension();
 	int width = win.get_width();
 	int height = win.get_height();
 	double ox = win.get_ox() - dim;
 	double oy = win.get_oy() - dim / width * height;
-	int N = 100;
+	int N = 500;
 
 	char *cache = new char[width * height * 3];
 
 	for (int j = 0; j < height; j++)
 		for (int i = 0; i < width; i++)
 		{
-		  //int bdryn = 0;
+		  int bdryn = 0;
+		  int flagn = rand() % 500;
+		  int prtflag = 0;
 		  
 			double x = ox + lpp * i;
 			double y = oy + lpp * j;
 			int pos = width * j + i;
-			int itn = 0;
+			int itn = 0;  //我在这里加入了一个计数器，用来记录迭代的次数。再次迭代时会初始化。
 			Manderbrot man(std::complex<double>{0.0, 0.0},
 						   N,
 						   std::complex<double>{x, y});
@@ -43,32 +45,43 @@ int main(int argc, char *argv[])
 				if (man.is_disconvergence())
 					break;
 			}
-			/*
-			//points that disconverge
-		       	if (man.stop_criterion())
-			{
-				cache[pos * 3] = 255 - itn * 2.55;
-				cache[pos * 3 + 1] = 255 - itn * 2.55;
-				cache[pos * 3 + 2] = 255 - itn * 2.55;
-			}
-			//points that converge
-			else
-			{
-				cache[pos * 3] = 0;
-				cache[pos * 3 + 1] = 0;
-				cache[pos * 3 + 2] = 0;
-		       	}
-			*/
+			//以下是更改的颜色参数。
 			cache[pos * 3] = 255;
-			cache[pos * 3 + 1] = (itn - 200 ) * (itn - 200)* (itn - 200)* (itn - 200)* (itn - 200)* (itn - 200) *0.03;
-			cache[pos * 3 + 2] = (itn - 200 ) * (itn - 200)* (itn - 200)* (itn - 200)* (itn - 200)* (itn - 200) *0.1;
-			/*if(bdryn < 500 && itn == 99 && y == 0 && x > 0.25){
+			cache[pos * 3 + 1] = itn * itn * itn * 0.03;
+			cache[pos * 3 + 2] = itn * itn * itn * 0.1;
+			
+			if(bdryn < 500 && itn >= N - 50 && prtflag == 0){
+			  if(bdryn == flagn){
+			    o_x = x;
+			    o_y = y;
+			  }
 			  bdryn++;
-			  std::cout<<'('<<x<<','<<y<<')'<<std::endl;
-			  }*/
+			  prtflag = 1;
+			  }
 			
 		}
-	build_bmp(argv[1], width, height, cache);
+	build_bmp(bmpname, width, height, cache);
 	delete [] cache;
 	return 0;
 };
+
+int main(int argc, char *argv[]){
+  //初始输入无需命名文件，但要给出坐标和初始dimension。
+	if (argc < 3)
+	{
+		std::cerr << "Usage: " << argv[0]
+				  << "ox oy dimension" << std::endl;
+		exit(-1);
+	}
+	int i;
+	o_x = std::atof(argv[1]);
+	o_y = std::atof(argv[2]);
+	double dim = std::atof(argv[3]);
+	char name[9] = "run0.bmp";   //每次运行创建一个名为run_.bmp的文件，空格处为第几次放大。
+	for (i = 0; i < 10; i++){
+	  generate(name, o_x, o_y, dim);
+	  name[3]++;
+	  dim /= 4 ;                 //每次放大4倍。（考虑到电脑性能T_T)
+	  std::cout<<"OUtput"<<std::endl;
+	}
+}
